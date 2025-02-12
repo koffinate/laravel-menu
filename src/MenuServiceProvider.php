1ec0a225ea4d26049d95c78eea739b2fd98248bb
Much Yusron Arif <yusron.arif4@gmail.com>
2025-02-12 15:22:59 +0700

    add config for menu icon

--- src/MenuServiceProvider.php
+++ src/MenuServiceProvider.php
@@ -13,7 +13,7 @@ class MenuServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->mergeConfigFrom(__DIR__.'/../config/menu-icon.php', 'koffinate.menu-icon');
     }
 
     /**
@@ -25,5 +25,7 @@ class MenuServiceProvider extends ServiceProvider
     {
         $this->app->singleton('menus', fn ($app, $p) => new Factory(name: $p['name'] ?: null));
         $this->app->alias('menus', Factory::class);
+
+        $this->publishes([__DIR__.'/../config/menu-icon.php' => config_path('koffinate/menu-icon.php')], 'kfn-config-menu');
     }
 }
